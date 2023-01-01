ee012c867a8e59a72fc80b682f0e2ca4d2185fd4
Mary Farag <53305852+mfarag2@users.noreply.github.com>
2023-01-01 16:11:36 -0500

    initial commit

---
+++ cacheBlocking/cacheBlocking.c
@@ -0,0 +1,100 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <time.h>
+
+/* Markers used to bound trace regions of interest */
+volatile char MARKER_START, MARKER_END;
+
+int main(int argc, char* argv[])
+{
+
+    /* Record marker addresses */
+    FILE* marker_fp = fopen(".marker","w");
+    assert(marker_fp);
+    fprintf(marker_fp, "%llx\n%llx",
+        (unsigned long long int) &MARKER_START,
+        (unsigned long long int) &MARKER_END );
+    fclose(marker_fp);
+
+    if (argc!=3) {
+        printf("Usage: ./cacheBlocking <matrix_a_file> <matrix_b_file>\n");
+        exit(EXIT_FAILURE);
+    }
+
+    FILE* matrix_a_fp = fopen(argv[1], "r");
+    if (!matrix_a_fp) {
+        perror("fopen failed");
+        return EXIT_FAILURE;
+    }
+
+    size_t n;
+    int ret = fscanf(matrix_a_fp, "%ld\n", &n);
+    assert (ret==1);
+    int* a = calloc( n*n, sizeof(int) );
+    for ( size_t i=0; i<n; i++ ) {
+        for ( size_t k=0; k<n; k++ ) {
+            ret = fscanf(matrix_a_fp, "%d ", &a[i*n+k]);
+            assert (ret==1);
+        }
+        ret = fscanf(matrix_a_fp, "\n");
+    }
+    fclose(matrix_a_fp);
+
+    FILE* matrix_b_fp = fopen(argv[2], "r");
+    if (!matrix_b_fp) {
+        perror("fopen failed");
+        return EXIT_FAILURE;
+    }
+    size_t m;
+    ret = fscanf(matrix_b_fp, "%ld\n", &m);
+    assert (ret==1);
+    assert( n==m );
+    int* b = calloc( n*n, sizeof(int) );
+    for ( size_t k=0; k<n; k++ ) {
+        for ( size_t j=0; j<n; j++ ) {
+            ret = fscanf(matrix_b_fp, "%d ", &b[k*n+j]);
+            assert (ret==1);
+        }
+        ret = fscanf(matrix_b_fp, "\n");
+    }
+    fclose(matrix_b_fp);
+
+    int* c = calloc( n*n, sizeof(int) );
+
+    int block_size = 4;
+
+    MARKER_START = 211;
+
+    double r;
+    for (int i = 0; i < n; i += block_size) {
+            for (int j = 0; j < n; j += block_size) {
+                    for (int k = 0; k < n; k += block_size) {
+                            for (int i1 = i; i1 < (i+block_size) ; i1++) {
+                                    for (int j1 = j; j1 < (j+block_size); j1++) {
+                                            r = a[j1 * n + i1];
+                                            for (int k1 = k; k1 < (k+block_size); k1++){
+                                                    c[j1 * n + k1] += r * b[i1 * n + k1];
+                                            }
+                                    }
+                            }
+                    }
+            }
+    }
+
+    MARKER_END = 211;
+
+    for ( size_t i=0; i<n; i++ ) {
+        for ( size_t j=0; j<n; j++ ) {
+            printf( "%d ", c[i*n+j] );
+        }
+        printf( "\n" );
+    }
+
+    free(c);
+    free(b);
+    free(a);
+    exit(EXIT_SUCCESS);
+
+}
